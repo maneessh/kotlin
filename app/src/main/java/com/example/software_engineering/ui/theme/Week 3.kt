6b3436ed25a2109dc6a2945c4134246ef1f97599
Manish Rumba <manishrumba@Manish-ka-MacBook-Pro.local>
2025-04-20 12:45:15 +0545

    week1

--- app/src/main/java/com/example/software_engineering/ui/theme/Week 3.kt
+++ app/src/main/java/com/example/software_engineering/ui/theme/Week 3.kt
@@ -1,2 +1,19 @@
 package com.example.software_engineering.ui.theme
 
+
+fun main() {
+//mutable Variable
+    var age = 100;
+    age = 20
+
+//immutable Varibale
+    val speed = 100
+    speed = 10
+
+
+
+
+
+}
+
+
